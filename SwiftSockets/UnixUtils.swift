02d5ef87dcc48016355d954f765960816c573905
Helge Hess <helge@alwaysrightinstitute.com>
2016-03-10 16:16:10 +0100

    Added sysXYZ aliases for Glibc vs Darwin system calls. (e.g. use sysRead() instead of Glibc.read())

--- SwiftSockets/UnixUtils.swift
+++ SwiftSockets/UnixUtils.swift
@@ -25,6 +25,10 @@ let htons = ntohs // same thing, swap bytes :-)
 
 // MARK: - ioctl / ioccom stuff
 
+#if os(Linux)
+let sysFIONREAD : CUnsignedLong = CUnsignedLong(Glibc.FIONREAD)
+#else /* os(Darwin) */
+// TODO: still required?
 let IOC_OUT  : CUnsignedLong = 0x40000000
 
 // hh: not sure this is producing the right value
@@ -32,9 +36,10 @@ let FIONREAD : CUnsignedLong =
   ( IOC_OUT
   | ((CUnsignedLong(sizeof(Int32)) & CUnsignedLong(IOCPARM_MASK)) << 16)
   | (102 /* 'f' */ << 8) | 127)
+let sysFIONREAD = FIONREAD
+#endif /* os(Darwin) */
 
-
-/* dispatch convenience */
+// MARK: - dispatch convenience
 
 import Dispatch
 
@@ -50,10 +55,12 @@ extension dispatch_source_t {
 
 
 // MARK: - Replicate C shims - BAD HACK
+// TODO: not required anymore? varargs work on Linux?
+//       but not in Xcode yet?
 
-private let dlHandle = Darwin.dlopen(nil, RTLD_NOW)
-private let fnFcntl  = Darwin.dlsym(dlHandle, "fcntl")
-private let fnIoctl  = Darwin.dlsym(dlHandle, "ioctl")
+private let dlHandle = sysDlopen(nil, RTLD_NOW)
+private let fnFcntl  = sysDlsym(dlHandle, "fcntl")
+private let fnIoctl  = sysDlsym(dlHandle, "ioctl")
 
 typealias fcntlViType  =
     @convention(c) (Int32, Int32, Int32) -> Int32
@@ -74,3 +81,62 @@ func ari_ioctlVip(fildes: Int32, _ cmd: CUnsignedLong,
   let fp = unsafeBitCast(fnIoctl, ioctlVipType.self)
   return fp(fildes, cmd, val)
 }
+
+
+// MARK: - Wrap system naming differences
+
+#if os(Linux)
+import Glibc
+
+public enum POSIXError : CInt {
+  case EPERM
+}
+
+typealias sysOpenType = (UnsafePointer<CChar>, CInt) -> CInt
+let sysOpen        : sysOpenType = Glibc.open
+let sysClose       = Glibc.close
+let sysRead        = Glibc.read
+let sysWrite       = Glibc.write
+let sysPoll        = Glibc.poll
+let sysBind        = Glibc.bind
+let sysConnect     = Glibc.connect
+let sysListen      = Glibc.listen
+let sysAccept      = Glibc.accept
+let sysShutdown    = Glibc.shutdown
+let sysGetsockname = Glibc.getsockname
+let sysGetpeername = Glibc.getpeername
+let sysDlsym       = Glibc.dlsym
+let sysDlopen      = Glibc.dlopen
+
+var sysErrno : Int32 { return Glibc.errno }
+
+let sys_SOCK_STREAM : Int32 = Int32(SOCK_STREAM.rawValue)
+let sys_SOCK_DGRAM  : Int32 = Int32(SOCK_DGRAM.rawValue)
+let sys_SHUT_RD     : Int32 = Int32(SHUT_RD)
+
+#else // os(Darwin)
+
+import Darwin
+
+let sysOpen        = Darwin.open
+let sysClose       = Darwin.close
+let sysRead        = Darwin.read
+let sysWrite       = Darwin.write
+let sysPoll        = Darwin.poll
+let sysBind        = Darwin.bind
+let sysConnect     = Darwin.connect
+let sysListen      = Darwin.listen
+let sysAccept      = Darwin.accept
+let sysShutdown    = Darwin.shutdown
+let sysGetsockname = Darwin.getsockname
+let sysGetpeername = Darwin.getpeername
+let sysDlsym       = Darwin.dlsym
+let sysDlopen      = Darwin.dlopen
+
+var sysErrno : Int32 { return Darwin.errno }
+
+let sys_SOCK_STREAM = SOCK_STREAM
+let sys_SOCK_DGRAM  = SOCK_DGRAM
+let sys_SHUT_RD     = SHUT_RD
+
+#endif // os(Darwin)
