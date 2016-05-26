5cc77cf3d2f86e2a36ad9aff7c545618acee8795
Helge Hess <helge@alwaysrightinstitute.com>
2016-05-26 12:31:27 +0200

    More complex travising.

---
+++ .travis/install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# URLS
+
+TT_SWIFTENV_URL="https://github.com/kylef/swiftenv.git"
+TT_GCD_URL="https://github.com/apple/swift-corelibs-libdispatch.git"
+
+TT_GCD_SWIFT3_BRANCH=experimental/foundation
+
+# swiftenv
+
+git clone --depth 1 ${TT_SWIFTENV_URL} ~/.swiftenv
+
+SWIFTENV_ROOT="$HOME/.swiftenv"
+
+PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"
+
+
+# Install Swift
+
+swiftenv install ${SWIFT_SNAPSHOT_NAME}
+
+
+# Environment
+
+TT_SWIFT_BINARY=`swiftenv which swift`
+TT_SNAP_DIR=`echo $TT_SWIFT_BINARY | sed "s|/usr/bin/swift||g"`
+
+
+# Install GCD
+
+git clone --recursive ${TT_GCD_URL} gcd
+cd gcd
+git checkout ${TT_GCD_SWIFT3_BRANCH}
+git pull
+./autogen.sh
+
+./configure --with-swift-toolchain=${TT_SNAP_DIR}/usr --prefix=${TT_SNAP_DIR}/usr
+make
+make install
