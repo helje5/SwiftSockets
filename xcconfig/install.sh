3f030f36a915bcf9426da1855c65852c53921771
Helge Hess <helge@alwaysrightinstitute.com>
2016-05-27 13:38:31 +0200

    Use master for Swift3, link some home-system setup.

--- xcconfig/install.sh
+++ xcconfig/install.sh
@@ -5,8 +5,9 @@
 TT_SWIFTENV_URL="https://github.com/kylef/swiftenv.git"
 TT_GCD_URL="https://github.com/apple/swift-corelibs-libdispatch.git"
 
-TT_GCD_SWIFT3_BRANCH=experimental/foundation
+#TT_GCD_SWIFT3_BRANCH=experimental/foundation
 #TT_GCD_SWIFT22_1404_HASH=65330e06d9bbf75a4c6ddc349548536746845059
+TT_GCD_SWIFT3_BRANCH=master
 TT_GCD_SWIFT22_1404_HASH=master
 
 # swiftenv
@@ -48,6 +49,9 @@ if [[ "$TRAVIS_OS_NAME" == "Linux" ]]; then
   else
     git checkout ${TT_GCD_SWIFT22_1404_HASH}
   fi
+  
+  mkdir ~/swift-not-so-much
+  ln -s ${TT_SNAP_DIR} ~/swift-not-so-much/latest
   
   export CC=clang
   ./autogen.sh
